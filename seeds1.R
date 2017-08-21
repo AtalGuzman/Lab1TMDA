937951f6d6195a0c49301b8308932f884db613d5
Adolfo Guzmán <adolfo.guzman@usach.cl>
2017-08-21 10:42:21 -0300

    no lo sé

--- seeds1.R
+++ seeds1.R
@@ -1,11 +1,24 @@
 require(mclust)
 require(Hmisc)
-require(ggplots)
+require(ggplot2)
+
+#Funciones utilizadas
+QQPLOT <- function(data){
+  
+  sample <- data[,1:7]
+  x <- qnorm(c(0.25,0.75))
+  y <- quantile(data[,1:7], c(0.25,0.75))
+  m <- diff(y)/diff(x)
+  int <- y[1] - m * x[1]
+  p <- ggplot(data, aes(sample = data[,1:7]))+
+    stat_qq()+
+    geom_abline(slope = m, intercept = int,color = "blue")
+  p
+  }
 
 #Lectura del archivo
-seedsCC <- read.table("C:\\Users\\Natalia\\Google Drive\\2017 - 2\\Minería de Datos Avanzada\\IX.- grupo 4 seeds\\seeds_dataset.txt", 
+seedsCC <- read.table("C:\\Users\\usuario\\Documents\\1.- Universidad\\nivel 10\\Taller de minería de datos avanzada\\Laboratorio 1\\seeds_dataset.txt", 
                       col.names = c("A","P","C","LoK","WoK","AC","LoKG","Class"))
-
 
 #Limpieza de datos
 #   Encontrar datos pérdidos
@@ -27,8 +40,16 @@ seedsSC <- seedsCC[,1:7]
 
 #Análisis estadístico
 #Descripción básica de los datos
+
 describe(seedsSC)
 
 #Test de normalidad
-qqline(seedsSC[1:7])
+#Se prueba gráficamente
 
+sample <- seedsSC[,1]
+x <- qnorm(c(0.25,0.75))
+y <- quantile(sample, c(0.25,0.75))
+m <- diff(y)/diff(x)
+int <- y[1] - m * x[1]
+
+p <- ggplot(seedsSC, aes(sample = sample))+stat_qq()+geom_abline(slope = m, intercept = int,color = "blue")
